1b0865ff6f935ac9ee334c6036726c4b7fb69953
Kilwa YT <ilyashilane@gmail.com>
2023-11-25 20:19:39 +0100

    hh

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbar = new CircularProgressBar.CircularProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pbar
+            // 
+            this.pbar.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.pbar.AnimationSpeed = 500;
+            this.pbar.BackColor = System.Drawing.Color.Transparent;
+            this.pbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.pbar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pbar.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.pbar.InnerMargin = 2;
+            this.pbar.InnerWidth = -1;
+            this.pbar.Location = new System.Drawing.Point(255, 92);
+            this.pbar.MarqueeAnimationSpeed = 2000;
+            this.pbar.Name = "pbar";
+            this.pbar.OuterColor = System.Drawing.Color.Gray;
+            this.pbar.OuterMargin = -25;
+            this.pbar.OuterWidth = 26;
+            this.pbar.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(186)))), ((int)(((byte)(242)))));
+            this.pbar.ProgressWidth = 15;
+            this.pbar.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.pbar.Size = new System.Drawing.Size(292, 288);
+            this.pbar.StartAngle = 270;
+            this.pbar.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.pbar.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.pbar.SubscriptText = "";
+            this.pbar.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.pbar.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.pbar.SuperscriptText = "";
+            this.pbar.TabIndex = 0;
+            this.pbar.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.pbar.Value = 68;
+            this.pbar.Click += new System.EventHandler(this.pbar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbar);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private CircularProgressBar.CircularProgressBar pbar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
